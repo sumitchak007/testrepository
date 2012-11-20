72107631a0763351458e33903e1782e3ebb895f4
Sumit Chakraborty <sumit.chakraborty@happiestminds.com>
2012-11-20 14:44:22 +0530

    second commit

--- testrepository/src/testpackage/Firsttest.java
+++ testrepository/src/testpackage/Firsttest.java
@@ -8,6 +8,7 @@ public class Firsttest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("This is my first test in Github");
+		System.out.println("This is my 2nd test in Github");
 	}
 
 }
