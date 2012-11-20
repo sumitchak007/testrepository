6539fd91bcbf3c68b90ad8b4f0de3131985f5619
Sumit Chakraborty <sumit.chakraborty@happiestminds.com>
2012-11-20 14:36:35 +0530

    Initial commit

---
+++ testrepository/src/testpackage/Firsttest.java
@@ -0,0 +1,13 @@
+package testpackage;
+
+public class Firsttest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is my first test in Github");
+	}
+
+}
